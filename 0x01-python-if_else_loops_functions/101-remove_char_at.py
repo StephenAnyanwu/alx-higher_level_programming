25a9afcf36df5a53e2e70abcc7112075da38fc97
StephenAnyanwu <voltasteve@gmailcom>
2023-11-29 02:26:46 -0800

    task 15 third commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
 creates a copy of the string, removing the character at the position n
+(not the Python way, the “C array index”).
 """
 
 
 def remove_char_at(str, n):
     new_str = ""
-    for i in range(len(str + 1)):
-        if i != n:
-            new_str += str[i]
-        else:
-            new_str += ' '
-    return new_str
+    if n >= 0:
+        for i in range(len(str)):
+            if i != n:
+                new_str += str[i]
+        return new_str
+    return (str)
