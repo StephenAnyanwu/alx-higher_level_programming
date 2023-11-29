85c5d1f3bca0cee4f352e8e323860342b71562b2
StephenAnyanwu <voltasteve@gmailcom>
2023-11-29 10:06:12 -0800

    task 13 second commit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,6 +28,41 @@ void sort_list(listint_t **head)
 }
 
 /**
+ * add_node_beginning - add node at the beginning.
+ * @head: pointer to pointer of first node of listint_t list.
+ * @new_node: new node to add.
+*/
+void add_node_beginning(listint_t **head, listint_t *new_node)
+{
+	new_node->next = *head;
+	*head = new_node;
+
+}
+
+/**
+ * add_node_middle - add node at the middle.
+ * @ptr1: first pointer to a node.
+ * @ptr2: second pointer to a node.
+ * @new_node: new node to add.
+*/
+void add_node_middle(listint_t *ptr1, listint_t *ptr2, listint_t *new_node)
+{
+	ptr1->next = new_node;
+	new_node->next = ptr2;
+}
+
+/**
+ * add_node_last - add a node at the end.
+ * @last_node: pointer to the last node.
+ * @new_node: new node to add.
+*/
+void add_node_last(listint_t *last_node, listint_t *new_node)
+{
+	if (last_node->next == NULL)
+		last_node->next = new_node;
+}
+
+/**
 * insert_node - a function that inserts a number into a
 * sorted singly linked list.
 * @head: pointer to pointer of first node of listint_t list
@@ -49,14 +84,15 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 	sort_list(head);
+	if (new->n < (*head)->n)
+		add_node_beginning(head, new);
 	ptr1 = *head;
 	ptr2 = (*head)->next;
 	while (ptr2 != NULL)
 	{
-		if (ptr2->n > new->n)
+		if (ptr2->n > new->n || ptr2->n > new->n)
 		{
-			ptr1->next = new;
-			new->next = ptr2;
+			add_node_middle(ptr1, ptr2, new);
 			return (new);
 		}
 		ptr1 = ptr1->next;
@@ -64,7 +100,6 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	/* if we reach the last node, meaning all the values of the */
 	/* nodes are less than number, thus we append the new node. */
-	ptr2->next = new;
+	add_node_last(ptr2, new);
 	return (new);
-
 }
