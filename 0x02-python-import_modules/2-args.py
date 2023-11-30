40c04e83f5e0d6b3a4f21b9da171e6c8ee59cb79
StephenAnyanwu <voltasteve@gmailcom>
2023-11-30 04:37:43 -0800

    task 2 corr

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{} arguments.".format(0))
     if len(sys.argv) == 2:
-        print("{} argument.".format(1))
+        print("{} argument:".format(1))
         print("{}: {}".format(1, sys.argv[1]))
     if len(sys.argv) > 2:
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
