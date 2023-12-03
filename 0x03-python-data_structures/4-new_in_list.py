0bdf9eb2333c272b5e8dc38940459e9322694932
StephenAnyanwu <voltasteve@gmailcom>
2023-12-02 16:27:08 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """
+    replaces an element in a list at a specific
+    position without modifying the original list
+
+    Paramters
+    ---------
+    my_list : list
+        contains the element to be replaced
+    idx : int
+        index of the element to be replaced
+    element
+        new element
+
+    Returns
+    -------
+    list
+        new list with replaced element.
+    """
+    new_list = my_list
+    new_list[i] = element
+    return (new_list)
