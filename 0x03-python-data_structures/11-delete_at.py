51ce0b0dce1b8dee9b5783b6d7e86c3c8092c454
StephenAnyanwu <voltasteve@gmailcom>
2023-12-03 06:14:02 -0800

    task 11 third commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -22,5 +22,5 @@ def delete_at(my_list=[], idx=0):
         return (my_list)
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    my_list[idx] = []
+    my_list[idx:idx + 1] = []
     return (my_list)
