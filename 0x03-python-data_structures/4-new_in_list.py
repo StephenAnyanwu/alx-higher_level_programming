fee1e3eaeacc2f434aa76cf46049d78ff487157a
StephenAnyanwu <voltasteve@gmailcom>
2023-12-02 16:34:45 -0800

    task 4 second commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -24,5 +24,5 @@ def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1:
         return (my_list)
     new_list = my_list
-    new_list[i] = element
+    new_list[idx] = element
     return (new_list)
