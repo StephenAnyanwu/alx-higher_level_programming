cf643a3424ef0cb2d51b0e54c1da9808ae449fd5
StephenAnyanwu <voltasteve@gmailcom>
2023-12-02 17:59:27 -0800

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    returns a tuple with the length
+    of a string and its first character
+
+    Parameter
+    --------
+    sentence : str
+        string to return its first character
+    Returns
+    -------
+    tuple
+        length of a string and its first character
+    """
+    if len(sentence) != 0:
+        return (len(sentence, sentence[0]))
+    return (0, None)
