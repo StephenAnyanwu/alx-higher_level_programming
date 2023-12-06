28a78db52812d812f5c30111a89ebd436dd6cdea
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 08:50:23 -0800

    task 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+     replaces all occurrences of an element by another
+     in a new list
+
+     Parameters
+     ----------
+     my_list : list
+        the initial list
+    search
+        the  element to replace in the list
+    replace
+        the new element
+
+    Returns
+    -------
+    list
+        a new list
+    """
+
+    serach_idx = my_list.index(search)
+    new_list = my_list
+    new_list[search_idx] = replace
+    return (new_list)
