3993a87eed8e1f8002a581a984f8cd13c3c3f40e
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 18:10:03 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -11,7 +11,7 @@ def best_score(a_dictionary):
     str
         key with the biggest integer valur, or None is not found
     """
-    if a_dictionary is None:
+    if (a_dictionary is None) or len(a_dictionary) == 0:
         return (None)
     val = 0
     key = ""
