dca1fabbb3b825e9060a680174ab1ef0fcc6a01e
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 17:27:59 -0800

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """"
+     returns a new dictionary with all values multiplied by 2
+
+     Paramter
+     --------
+     a_dictionary : dict
+    """
+
+    new_dict = {}
+    for key in new_dict:
+        new_dict[key] = a_dictionary[key] * 2
+    return (new_dict)
