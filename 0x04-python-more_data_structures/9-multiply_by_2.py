fdfd797727b9b10bfd742293630e5b46817140a6
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 17:31:57 -0800

    task 9 second commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -10,6 +10,6 @@ def multiply_by_2(a_dictionary):
     """
 
     new_dict = {}
-    for key in new_dict:
-        new_dict[key] = a_dictionary[key] * 2
+    for key, val in a_dictionary.items():
+        new_dict[key] = val * 2
     return (new_dict)
