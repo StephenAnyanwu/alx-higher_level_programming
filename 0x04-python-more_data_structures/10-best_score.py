cbff897880bba5704cc73084b6eb57887929f6f0
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 18:07:03 -0800

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    finds a key of a dictionary with the biggest integer value
+
+    a_dictionary : dict
+
+    Returns
+    -------
+    str
+        key with the biggest integer valur, or None is not found
+    """
+    if a_dictionary is None:
+        return (None)
+    val = 0
+    key = ""
+    for k, v in a_dictionary.items():
+        if v < val:
+            val = v
+            key = k
+    for k, v in a_dictionary.items():
+        if v > val:
+            val = v
+            key = k
+    return (key)
