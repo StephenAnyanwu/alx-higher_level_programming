8818baa2dc0da15b432d827507e1f1cb7ca0219e
StephenAnyanwu <voltasteve@gmailcom>
2023-12-06 17:16:25 -0800

    task 8 second commit

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -16,6 +16,7 @@ def simple_delete(a_dictionary, key=""):
     dict
         a new dictionary
     """
-
+    if key not in a_dictionary:
+        return (a_dictionary)
     del a_dictionary[key]
     return (a_dictionary)
