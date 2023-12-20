bee84fb26ca8e910b65903b235378a4382eb45c7
StephenAnyanwu <voltasteve@gmailcom>
2023-12-20 14:47:26 -0800

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Classes and Objects
+This module demostrates how an empty class is defined.
+The class in this module:
+    * Square - an empty class that defines a square.
+"""
+
+
+class Square:
+    """an empty class that defines a square."""
+    pass
