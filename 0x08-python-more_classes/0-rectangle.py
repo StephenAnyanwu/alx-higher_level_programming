ab79967a0ec520c540fdc50b25016d46f3a9bcdc
StephenAnyanwu <voltasteve@gmailcom>
2024-01-04 12:23:10 -0800

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Classes and Objects
+This module demostrates how an empty class is defined.
+The class in this module:
+    * Rectangle - an empty class that defines a square.
+"""
+
+
+class Rectangle:
+    """an empty class that defines a rectangle"""
+    pass
