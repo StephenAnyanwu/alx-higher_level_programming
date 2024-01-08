296d43fc7bfe8fd47e5c421cfed7d88470f70afa
StephenAnyanwu <voltasteve@gmailcom>
2024-01-08 15:18:11 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+In this module defines a function 'lookup'
+"""
+
+
+def lookup(obj):
+    """returns the list of available attributes
+    and methods of an object.
+
+    Parameters
+    ----------
+    obj : any type
+        the object
+    """
+    return dir(obj)
+
+
+if __name__ == "__main__":
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
