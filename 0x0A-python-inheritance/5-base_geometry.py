0530f3d266c3df9e83a933007f26fee4a1519a86
StephenAnyanwu <voltasteve@gmailcom>
2024-01-08 15:50:13 -0800

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+In this module define an empty class 'BaseGeometry'
+"""
+
+
+class BaseGeometry:
+    pass
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
