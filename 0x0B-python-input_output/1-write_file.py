4384a48c0775291a889ed3de762768098788f9fb
StephenAnyanwu <voltasteve@gmailcom>
2024-01-10 13:21:11 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+In this module defines a function 'write_file'.
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8),
+    overwrites the content of the file if it already
+    exists
+
+    Parameters
+    ----------
+    filename : str, optional
+        file to write to (default: empty string)
+    text : str, optional
+        string to write into filename (default: empty string)
+    
+    Returns
+    -------
+    int
+        the number of characters written into filename
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        num_char = f.write(text)
+        return num_char
+
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", \
+            "This School is so cool!\n")
+    print(nb_characters)
