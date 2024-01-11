bb640a4ac30b47b973e673cfca67e4ac0a23b86c
StephenAnyanwu <voltasteve@gmailcom>
2024-01-11 04:11:51 -0800

    task corr

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,17 +9,20 @@ exist. it's created.
 
 
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if len(sys.argv) > 1:
-    args = sys.argv[1:]
-else:
-    args = []
-try:
-    json_to_py = load_from_json_file('add_item.json')
-    new_py_list = json_to_py + args
-    save_to_json_file(new_py_list)
-except Exception:
-    save_to_json_file(args, 'add_item.json')
+if __name__ == "__main__":
+    save_to_json_file = \
+        __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+    else:
+        args = []
+    try:
+        json_to_py = load_from_json_file('add_item.json')
+        new_py_list = json_to_py + args
+        save_to_json_file(new_py_list)
+    except Exception:
+        save_to_json_file(args, 'add_item.json')
