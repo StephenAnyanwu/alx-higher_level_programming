45891ff7a40c8f1078e8390f4222cc9f9579fc0b
StephenAnyanwu <voltasteve@gmailcom>
2024-01-16 22:45:41 -0800

    test_base corr1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,8 +13,11 @@ from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    pass
-
+    """Unittest for testing the Base class"""
+    def test_base_no_arg(self):
+        """Test if base class has no arguments"""
+        b = Base()
+        self.assertEqual(b.id, b.id + 1)
 
 if __name__ == "__main__":
     unittest.main()
