1966d1e6036b7b9de94fd71f809be5fe0bacbc05
StephenAnyanwu <voltasteve@gmailcom>
2024-01-19 01:33:05 -0800

    base test corr2

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,6 +6,10 @@ In this module defines a unittest for rectangle.py
 
 
 import unittest
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestRectangle(unittest.TestCase):
