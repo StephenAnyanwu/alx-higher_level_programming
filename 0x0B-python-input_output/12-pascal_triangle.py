46cbad60cad4ae185c6f65859412594c04411a5d
StephenAnyanwu <voltasteve@gmailcom>
2024-01-21 01:52:11 -0800

    task 12 update1

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,91 +1,19 @@
 #!/usr/bin/python3
 
 """
-Defines Combination class, pascal_triangle function and
-print_triangle function
+Defines a pascal_triangle function
 """
-
-
-class Combination:
-    """
-    Impliment combination of integers
-
-    Attributes
-    ----------
-    __n : int
-        number of elements
-    __r : int
-        number of combinations
-    get : int
-        get the combination
-
-    Methods
-    -------
-    __factorial(x)
-        find and return the factorial of an integer recursively
-    __combination(m, n)
-        find and return the combination of  integers
-    """
-    def __init__(self, n, r):
-        """
-        parameters
-        ----------
-        n : int
-            number of elements
-        r : int
-            number of combinations of elements
-        """
-        self.__n = n
-        self.__r = r
-        self.get = self.__combination(n, r)
-
-    def __factorial(self, x):
-        """
-        find the factorial of an integer recursively
-
-        Parameters
-        ----------
-        x : int
-            number to find its factorial
-        Returns
-        -------
-        int
-            factorial of x
-        """
-        if x == 0 or x == 1:
-            return 1
-        return (x * self.__factorial(x - 1))
-
-    def __combination(self, m, n):
-        """
-        find the combination of  integers
-
-        Paramters
-        ---------
-        m : int
-            number of elements
-        n : int
-            number of combination of elements
-
-        Returns
-        -------
-        int
-            combination of m and n (mCn)
-        """
-        fact = self.__factorial
-        comb_m_n = fact(m) / (fact(m - n) * fact(n))
-        return int(comb_m_n)
 
 
 def pascal_triangle(n):
     """
-    return a list of lists of integers representing the
-    Pascal’s triangle of n
+    Create and return a list of lists of integers representing
+    the Pascal’s triangle of n
 
     Parameters
     ----------
     n : int
-        degree of the Pascsl's triangle
+        Degree (i.e size) of the the triangle
 
     Returns
     -------
@@ -93,23 +21,26 @@ def pascal_triangle(n):
         nested list representing a Pascal triangle of
         degree n (ie the length of the list)therwise
         an empty list if n <= 0
-
     """
-    pas_tr = []
     if n <= 0:
-        return pas_tr
-    for p in range(n):
-        pas_tr.append([Combination(p, q).get for q in range(p + 1)])
-    return pas_tr
-
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+        return []
+    triangle = [[1]]
+    while len(triangle) < n:
+        temp = triangle[-1]
+        tri_next_row = [1]
+        for i in range(len(temp) - 1):
+            tri_next_row.append(temp[i] + temp[i + 1])
+        tri_next_row.append(1)
+        triangle.append(tri_next_row)
+    return triangle
 
 
 if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
     print_triangle(pascal_triangle(5))
