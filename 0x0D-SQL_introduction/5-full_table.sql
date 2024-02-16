32e1b3f0f951ee9f0a90ba186bd83f6d1030903c
StephenAnyanwu <voltasteve@gmailcom>
2024-02-16 14:55:37 -0800

    task 5 corr1

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- of the table 'first_table' from the database 'hbtn_1c_0' in MySQL server.
 -- The database name will be passed as an argument of the mysql command.
 -- EXPLAIN or DESCRIBE statement not allowed.
-SHOW FULL COLUMNS FROM first_table
+SHOW FULL COLUMNS FROM first_table;
