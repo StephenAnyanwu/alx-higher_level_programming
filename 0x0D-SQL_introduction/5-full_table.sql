47dc822d2965c4f612a5fbc63bdcd5601b172661
StephenAnyanwu <voltasteve@gmailcom>
2024-02-16 14:59:26 -0800

    task 5 corr2

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- A script that prints the full description (that is the data structure)
--- of the table 'first_table' from the database 'hbtn_1c_0' in MySQL server.
+-- of the table 'first_table' from the database 'hbtn_0c_0' in MySQL server.
 -- The database name will be passed as an argument of the mysql command.
 -- EXPLAIN or DESCRIBE statement not allowed.
-SHOW FULL COLUMNS FROM first_table;
+SHOW FULL COLUMNS FROM first_table IN hbtn_0c_0;
