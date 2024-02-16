df547308caf53976a086a2565693b7cddeeccb81
StephenAnyanwu <voltasteve@gmailcom>
2024-02-16 14:40:35 -0800

    task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- A script that prints the full description (that is the data structure)
+-- of the table 'first_table' from the database 'hbtn_0c_0' in MySQL server.
+SHOW COLUMNS FROM hbtn_0c_0.first_table
