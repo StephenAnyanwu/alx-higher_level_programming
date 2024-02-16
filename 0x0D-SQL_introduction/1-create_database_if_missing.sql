0bdac8ee7f8cd291ff7e918aafe8116b9128ad92
StephenAnyanwu <voltasteve@gmailcom>
2024-02-16 13:26:45 -0800

    task 1 corr1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- A sript that creates the database 'hbtn_0c_0' in MySQL server
-CREATE DATABASE hbtn_0c_0;
+-- If the database hbtn_0c_0 already exists, the scrit will not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
