a08310c197e5cc4837d4cb04523bebd84a77197f
StephenAnyanwu <voltasteve@gmailcom>
2024-03-16 12:28:27 -0700

    task 9

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,22 +2,22 @@
 
 /* A class the defines a rectangle */
 class Rectangle {
-	constructor (w, h) {
-		if ((w > 0) && (h > 0)) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		let x = '';
-		for (let i = 0; i < this.width; i++) {
-			x += 'X';
-		}
-		for (let j = 0; j < this.height; j++) {
-			console.log(x);
-		}
-	}
+  print () {
+    let x = '';
+    for (let i = 0; i < this.width; i++) {
+      x += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(x);
+    }
+  }
 }
 
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* A function that returns the reversed version of a list */
+exports.esrever = function (list) {
+  let lastIdx = list.length - 1;
+  for (let i = 0; floor(lastIdx / 2); i++) {
+    const tmp = list[lastIdx];  
+    list[lastIdx] = list[i];
+    list[i] = tmp;
+    lastIdx--;
+  }
+}
