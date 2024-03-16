967b3ddce75ff8032ad4fc1d1ef69492a2cecd65
StephenAnyanwu <voltasteve@gmailcom>
2024-03-16 11:35:16 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,33 +2,33 @@
 
 /* A class the defines a rectangle */
 class Rectangle {
-	constructor (w, h) {
-		if ((w > 0) && (h > 0)) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		let x = '';
-		for (let i = 0; i < this.width; i++) {
-			x += 'X';
-		}
-		for (let j = 0; j < this.height; j++) {
-			console.log(x);
-		}
-	}
+  print () {
+    let x = '';
+    for (let i = 0; i < this.width; i++) {
+      x += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(x);
+    }
+  }
 
-	rotate () {
-		let tmp = this.width;
-		this.width = this.height;
-		this.height = tmp;
-	}
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
 
-	double () {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let x = '';
+    for (let i = 0; i < this.width; i++) {
+      x += c;
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(x);
+    }
+}
