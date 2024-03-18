e279223f0d4ba729048697e48dbcaefa94488948
StephenAnyanwu <voltasteve@gmailcom>
2024-03-18 08:54:32 -0700

    task 8 corr2

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,8 @@
 /* A function that returns the reversed version of a list */
 exports.esrever = function (list) {
   let lastIdx = list.length - 1;
-  for (let i = 0; i <= floor(lastIdx / 2); i++) {
+  const midIdx = floor(lastIdx / 2);
+  for (let i = 0; i <= midIdx; i++) {
     const tmp = list[lastIdx];  
     list[lastIdx] = list[i];
     list[i] = tmp;
