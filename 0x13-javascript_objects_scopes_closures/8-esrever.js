98ee1fbe0fff979f7d7de1869eb9ba65c62df77e
StephenAnyanwu <voltasteve@gmailcom>
2024-03-18 09:02:29 -0700

    task 8 corr3

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,10 +4,11 @@
 exports.esrever = function (list) {
   let lastIdx = list.length - 1;
   const midIdx = floor(lastIdx / 2);
-  for (let i = 0; i <= midIdx; i++) {
+  for (let i = 0; i < midIdx; i++) {
     const tmp = list[lastIdx];  
     list[lastIdx] = list[i];
     list[i] = tmp;
     lastIdx--;
   }
+  return list;
 };
