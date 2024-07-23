0f67ca7dc96e45b01ded19ec8ae616309ce8690c
voltasteve <27285224-voltasteve@users.noreply.replit.com>
2024-07-23 21:15:42 +0000

    task 0 corr

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
-        print("Body response")
+        print("Body response:")
         print(f"\t- type: {type(content)}")
         print(f"\t- type: {content}")
         print(f"\t- utf8 content: {content.decode('utf-8')}")
