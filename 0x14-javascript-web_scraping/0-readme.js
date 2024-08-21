fecfd5df86e354d4e30ea3caf142fa666dc1e113
Stephen Anyanwu <voltasteve@gmail.com>
2024-08-21 10:30:37 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // A script that reads and prints the content of a file.
 
-const file_sys = require('fs');
-file_sys.readFile(process.argv[2], 'utf-8',
+const fileSys = require('fs');
+fileSys.readFile(process.argv[2], 'utf-8',
   function (err, data) {
     if (err) {
       console.log(err);
